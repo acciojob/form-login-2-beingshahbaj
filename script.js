cc1270d215f84117c7d654c12c5cb84a2a9b9159
beingshahbaj <130038256+beingshahbaj@users.noreply.github.com>
2023-11-10 19:28:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.getElementById("form").addEventListener("submit", function(event){
+ event.preventDefault(); 
+ var firstname = document.getElementById("fname").value;
+ var lastname = document.getElementById("lname").value;
+ var phonenumber = document.getElementById("phone").value;
+ var emailid = document.getElementById("email").value;
+
+ alert("First Name: " + firstname + "\nLast Name: " + lastname + "\nPhone Number: " + phonenumber + "\nEmail ID: " + emailid);
+});
